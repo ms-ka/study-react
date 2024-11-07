a723f07442e6139e4c367f23e661dff639332ec7
marGo <ms-ka@o2.pl>
2024-11-07 11:12:32 +0100

    change in HeroCard

---
+++ src/components/gallery/HeroCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function HeroCard({ isDark, name, age, image, weapons }) {
+    return (
+        <article className={isDark ? 'hero-dark' : 'hero-light'}>
+            <h3>{name}</h3>
+            <p> {name} is {age} years old </p>
+            <img src={image} alt="" />
+            <p>
+            Hero weapons: {weapons.map((weapon, index)=> (
+                <span className='weapon' key={index}>|{weapon}</span>
+            ))}
+            </p>
+          </article>
+    );
+}
+export default HeroCard;
